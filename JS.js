ef6080fbba21b82de6dc8be81887deb31a7d77e2
Keshav <contact.keshavyadav@gmail.com>
2021-12-23 14:10:21 +0530

    Data types in JS

--- JS.js
+++ JS.js
@@ -18,3 +18,37 @@ let username= 'keshav'
 let username1= "keshav's laptop"
 console.log(username) //keshav
 */
+
+//data types
+
+//costants
+
+//-> value of const variable can not be changed
+
+//-> finding area of a circle: Area= πr2
+/*
+let radius = 5
+const pi = 3.14
+let area
+
+
+radius= 6 //-> change the value of previous define var
+//pi= 3.15 //-> can't be change as it's constant, throw an error
+
+
+area = pi * radius * radius
+console.log(area)
+*/
+
+
+//data types (are mainly divided into two parts, primitive and object)
+
+//primitive -> number, string, boolean, null, undefined, symbols
+//object -> data types other then primitive called object data types
+/*
+
+let data= 8 // number
+let name= 'keshav' // string
+
+console.log(typeof data) //check type of data
+*/
