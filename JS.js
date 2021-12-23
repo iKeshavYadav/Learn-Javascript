fb393c796ce8905d862349289d11e00c81838ad1
Keshav <contact.keshavyadav@gmail.com>
2021-12-23 14:18:09 +0530

    Conditional Statement

--- JS.js
+++ JS.js
@@ -259,3 +259,53 @@ let n= !result
 
 console.log(result, n) //false true
 */
+
+//conditional statement
+/*
+let num1= 3;
+let num2= 4;
+
+let result= num1 > num2;
+
+if(result)
+  console.log("num1 is greater");
+else
+  console.log("num2 is greater");
+
+console.log("Bye...");
+*/
+/*
+//by default every condition will have one statement
+//all statement inside {} means it belongs to the same condition 
+let num1= 6;
+let num2= 4;
+
+if(num1 > num2){
+  console.log("num1 is greater");
+}
+
+else{
+  console.log("num2 is greater");
+  console.log("yeee...");
+}
+console.log("Bye...");
+*/
+
+/*
+let num1= 10;
+let num2= 9;
+let num3= 7;
+
+if(num1 > num2 && num1 > num3){
+  console.log("num1 is greatest");
+}
+
+else if(num2 > num3){
+  console.log("num2 is greatest");
+}
+
+else{
+  console.log("num3 is greatest");
+}
+console.log("Bye...");
+*/
