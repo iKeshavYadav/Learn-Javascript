d175639f77ccb945ae7cf1d7bf81d24250932c6b
Keshav <contact.keshavyadav@gmail.com>
2021-12-23 14:08:39 +0530

    Variables in JS

---
+++ JS.js
@@ -0,0 +1,20 @@
+//variables in JS
+/*
+let num = 4
+console.log(num) //4
+console.log(num + 3) //7
+
+//let num = 9 //SyntaxError: Identifier 'num' has already
+num = 9
+console.log(num)
+*/
+
+//let num = 4 -> defining variable
+//num = 9 -> changing the value of variable
+
+//string in JS
+/*
+let username= 'keshav'
+let username1= "keshav's laptop"
+console.log(username) //keshav
+*/
