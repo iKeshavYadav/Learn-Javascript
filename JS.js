71860308c34aff29c706b8f5ad8821362b911f5d
Keshav <contact.keshavyadav@gmail.com>
2021-12-23 14:20:43 +0530

    Switch Statement

--- JS.js
+++ JS.js
@@ -334,3 +334,40 @@ result= num%2===0 ? "Odd" : "Even"
 //if the condition is true,it will execute 1st statement else 2nd
 console.log(result);
 */
+
+/*
+//switch statement
+
+// Mon - 7am
+// Tue - Thurs - 4am
+// Fri - 9am
+// Sat - Sun - 8am
+
+let day= "Monday";
+
+switch(day){
+  case "Monday":
+    console.log("7am");
+    break;
+    
+  case "Tuesday":
+  case "Wednesday":
+  case "Thursday":
+    console.log("4am");
+    break;
+    
+  case "Friday":
+    console.log("9am");
+    break;
+    
+  case "Saturday":
+  case "Sunday":
+    console.log("8am");
+    break;
+    
+  default:
+  console.log("Sleep...")
+}
+
+console.log("Good Morning!")
+*/
