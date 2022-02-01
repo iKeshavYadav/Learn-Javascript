7ed9f521065cc8c130f48696ce39556cc4b4544b
Keshav Yadav <Contact.KeshavYadav@gmail.com>
2022-02-01 14:03:10 +0530

    Inheritance between Classes : Object.create

--- JS.js
+++ JS.js
@@ -2256,3 +2256,35 @@ const satyam = new StudentsCl('Satyam Tiwari', 2003, ' Computer Science');
 satyam.introduce();
 satyam.calcAge();
 */
+
+
+//Inheritance between Classes : Object.create
+/*
+const PersonProto = {
+    calcAge() {
+        console.log(2037 - this.birthYear);
+    },
+
+    init(firstName, birthYear){
+        this.firstName = firstName;
+        this.birthYear = birthYear;
+    },
+};
+
+const keshav = Object.create(PersonProto);
+
+const StudentProto = Object.create(PersonProto);
+StudentProto.init = function(firstName, birthYear, course){
+    PersonProto.init.call(this, firstName, birthYear);
+    this.course = course;
+};
+
+StudentProto.introduce = function(){
+    console.log(`My name is ${this.fullName} and I study ${this.course}`);
+};
+
+const satyam = Object.create(StudentProto);
+satyam.init('Satyam', 2002, 'Computer Science');
+satyam.introduce();
+satyam.calcAge();
+*/
