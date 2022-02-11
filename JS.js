f73e02a6a66fedcf6710d2a6ab79815f71dec88f
Keshav Yadav <Contact.KeshavYadav@gmail.com>
2022-02-11 16:13:08 +0530

    Event loop

--- JS.js
+++ JS.js
@@ -2418,3 +2418,18 @@ getCountryData('portugal');
 }
 
 */
+
+/*
+//Event loop
+
+console.log('Test start');
+setTimeout(() => console.log('0 Sec timer'), 0);
+Promise.resolve('Resolved promise 1').then(res => console.log(res));
+
+Promise.resolve('Resolved promise 2').then(res => {
+  for (let i = 0; i < 1000000000; i++){}
+  console.log(res)
+});
+
+console.log('Test end') 
+*/
